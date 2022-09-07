a7d492694b159ee99eedfeac86b630d2e1bc6dd2
Daiana Andreis <85138381+DaianaAndreis@users.noreply.github.com>
2022-09-07 18:12:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+numeros = [45, 54, 59, 104, 73]
+max = 0
+i = 0
+
+for (i = 0; i < numeros.length; i++) {
+  if (max > numeros[i]) {
+    max
+  }
+  else {
+    max = numeros[i]
+  }
+
+}
+console.log(max)
